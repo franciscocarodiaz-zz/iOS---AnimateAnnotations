5866a7fc89b37ef653d004891a183bef46ec5b50
Francisco Caro Diaz <franciscocarodiaz@gmail.com>
2014-11-10 10:26:06 +0100

    Initial Commit

---
+++ AllThePhotoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AllThePhotoes
+//
+//  Created by Francisco Caro Diaz on 10/11/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AllThePhotoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AllThePhotoes
+//
+//  Created by Francisco Caro Diaz on 10/11/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
